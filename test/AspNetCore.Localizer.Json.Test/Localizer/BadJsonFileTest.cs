6f804e1c4bf26ca1a4bb48dbc4485747ffb6ae67
Alexandre Teixeira <ateixeira@askmethat.fr>
2025-03-27 17:06:09 +0100

    fix: empty name return nothing

--- test/AspNetCore.Localizer.Json.Test/Localizer/BadJsonFileTest.cs
+++ test/AspNetCore.Localizer.Json.Test/Localizer/BadJsonFileTest.cs
@@ -6,42 +6,56 @@ using System;
 using System.Globalization;
 using AspNetCore.Localizer.Json.JsonOptions;
 
-namespace AspNetCore.Localizer.Json.Test.Localizer
+namespace AspNetCore.Localizer.Json.Test.Localizer;
+
+[TestClass]
+public class BadJsonJsonFileTest
 {
-    [TestClass]
-    public class BadJsonJsonFileTest
+    private JsonStringLocalizer localizer = null;
+    public void InitLocalizer(CultureInfo cultureInfo)
     {
-        private JsonStringLocalizer localizer = null;
-        public void InitLocalizer(CultureInfo cultureInfo)
+        CultureInfo.CurrentUICulture = cultureInfo;
+        localizer = JsonStringLocalizerHelperFactory.Create(new JsonLocalizationOptions()
         {
-            CultureInfo.CurrentUICulture = cultureInfo;
-            localizer = JsonStringLocalizerHelperFactory.Create(new JsonLocalizationOptions()
+            DefaultCulture = new CultureInfo("en-US"),
+            SupportedCultureInfos = new System.Collections.Generic.HashSet<CultureInfo>()
             {
-                DefaultCulture = new CultureInfo("en-US"),
-                SupportedCultureInfos = new System.Collections.Generic.HashSet<CultureInfo>()
-                {
-                     new CultureInfo("fr-FR")
-                },
-                ResourcesPath = $"{AppContext.BaseDirectory}/path",
-                AdditionalResourcePaths = new[] { $"{AppContext.BaseDirectory}/path2", $"{AppContext.BaseDirectory}/path3" },
-                IsAbsolutePath = true,
-                IgnoreJsonErrors = true
-            }); ;
-        }
-
-        [TestMethod]
-        public void TestReadName1_AbsolutePath_StringLocation()
-        {
-            InitLocalizer(new CultureInfo("fr-FR"));
-
-            LocalizedString result = localizer.GetString("Name1");            
-
-            Assert.AreEqual("Mon Nom 1", result);
-
-            LocalizedString result2 = localizer.GetString("Name3");
-
-            Assert.AreEqual("Mon Nom 3", result2);
-        }
-
+                new CultureInfo("fr-FR")
+            },
+            ResourcesPath = $"{AppContext.BaseDirectory}/path",
+            AdditionalResourcePaths = new[]
+            {
+                $"{AppContext.BaseDirectory}/path2",
+                $"{AppContext.BaseDirectory}/path3"
+            },
+            IsAbsolutePath = true,
+            IgnoreJsonErrors = true
+        });
+        ;
     }
+
+    [TestMethod]
+    public void TestReadName1_AbsolutePath_StringLocation()
+    {
+        InitLocalizer(new CultureInfo("fr-FR"));
+
+        LocalizedString result = localizer.GetString("Name1");
+
+        Assert.AreEqual("Mon Nom 1", result);
+
+        LocalizedString result2 = localizer.GetString("Name3");
+
+        Assert.AreEqual("Mon Nom 3", result2);
+    }
+
+    [TestMethod]
+    public void TestReadName1_RelativePath_NullStringLocation()
+    {
+        InitLocalizer(new CultureInfo("fr-FR"));
+
+        LocalizedString result = localizer.GetString("");
+
+        Assert.AreEqual("", result);
+    }
+
 }
